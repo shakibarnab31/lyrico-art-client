e6c39d4751c89bae3dbd806759aeb6fbef8dfb3a
shakibarnab31 <arnab.muslim@gmail.com>
2021-10-04 19:17:50 +0600

    about added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import artImage from '../../images/image-57.jpg'
+import './About.css'
+import missionImage from '../../images/wooman.webp'
+import musicImage from '../../images/female-student-playing-piano (1).jpg'
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about us</h2>
+        <div className="">
+            <h2>About us</h2>
+            <div className=" art d-flex justify-content-center mt-5 mx-5">
+                <img className="mx-5" src={artImage} alt="" />
+                <div className="m-5 text-start">
+                    <p>Art School</p>
+                    <h1>A leader in <br />
+                        Educating Artist <br />
+                        Since 2010</h1>
+                    <p>We are providing best quality art teaching to the students since 2010.Many of our students participate various international events and started their cherish dream career as Artist.We are so proud for their success.</p>
+                </div>
+            </div>
+            <div className=" mission d-flex justify-content-center mt-5">
+
+                <div className="m-5 pt-5 text-start">
+                    <p>Mission</p>
+                    <h1>The heighst  <br />
+                        caliber of artistic <br />
+                        Education</h1>
+                    <p>We are providing best quality art teaching to the students since 2010.Many of our students participate various international events and started their cherish dream career as Artist.We are so proud for their success.</p>
+                </div>
+                <img className="mx-5 mt-5" src={missionImage} alt="" />
+            </div>
+            <div className="music d-flex justify-content-center mt-5 mx-5">
+                <img className="mx-5" src={musicImage} alt="" />
+                <div className="m-5 text-start">
+                    <p>Music School</p>
+                    <h1>A leader in <br />
+                        Educating Musician <br />
+                        Since 2010</h1>
+                    <p>We are providing best quality art teaching to the students since 2010.Many of our students participate various international events and started their cherish dream career as Artist.We are so proud for their success.</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <h4>our footers</h4>
+        <div className="footer py-2 mt-5">
+            <div className="d-flex justify-content-center m-5">
+
+                <div className="mx-5">
+                    <h3>Contact Us</h3>
+                    <p>Email: Lyrico@gmail.com</p>
+                    <p>Phone: 124546</p>
+                    <p>Mobile: +8801245554866</p>
+                </div>
+                <div className="mx-5">
+                    <h4>Address</h4>
+                    <p>Block C, Road-122</p>
+                    <p>Chealses Street, London</p>
+                    <p>England</p>
+                </div>
+                <div className="mx-5">
+                    <h4>Social Media</h4>
+                </div>
+
+            </div>
+            <p className="text-center">copyright protected</p>
         </div>
     );
 };
